eccf9e6c3a280813573051e66f597c153d3f9ccf
a.gorulev <a.gorulev@kernelics.com>
2021-10-20 20:06:23 +0000

    Updated script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ DESTIONATION_URL=git@github.com:SCSI-9/test.git
 SOURCE_URL=git@ssh.dev.azure.com:v3/paidpiper2020/PaidPiper
 
 
-sed -i "s|$SOURCE_URL|$DESTIONATION_URL|g" .gitmodules
+sed -i "s|$SOURCE_URL|$DESTIONATION_URL|g" gitmodules
