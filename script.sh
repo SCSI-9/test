4f22aa481729abec22fe86352a0dcff6e8a01ebc
a.gorulev <a.gorulev@kernelics.com>
2021-10-20 20:05:00 +0000

    Added script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DESTIONATION_URL=git@github.com:SCSI-9/test.git
+SOURCE_URL=git@ssh.dev.azure.com:v3/paidpiper2020/PaidPiper
+
+
+sed -i "s|$SOURCE_URL|$DESTIONATION_URL|g" .gitmodules
