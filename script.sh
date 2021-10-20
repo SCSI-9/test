abfd10a27c999e0f2d6c716088e6df74a14504f0
a.gorulev <a.gorulev@kernelics.com>
2021-10-20 21:31:52 +0000

    Updated script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 DESTIONATION_URL=git@github.com:SCSI-9/test.git
 SOURCE_URL=git@ssh.dev.azure.com:v3/paidpiper2020/PaidPiper
 find . -type f -name "newfile1" -print0 | xargs -0 sed -i 's+helllo+git@github.com:SCSI-9/test.git/+g'
-cat gitmodules
+cat newfile1
