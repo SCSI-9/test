87c06358f52199c605be3bc4f3848e961dc5ac7a
a.gorulev <a.gorulev@kernelics.com>
2021-10-21 05:28:12 +0000

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-export DESTIONATION_URL=git@github.com:SCSI-9/test.git
-export SOURCE_URL=git@ssh.dev.azure.com:v3/paidpiper2020/PaidPiper
-#sed -i 's+$SOURCE_URL+$DESTIONATION_URL/+g' /home/vsts/work/1/s/gitmodules
-cat gitmodules
+sed -i 's/file/newfile/g' newfile1
