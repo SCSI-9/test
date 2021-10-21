307a706890f8ea9e4bafa367a6144f5923f0716b
a.gorulev <a.gorulev@kernelics.com>
2021-10-21 05:54:49 +0000

    Updated script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export DESTIONATION_URL=git@github.com:SCSI-9/test.git
 export SOURCE_URL=git@ssh.dev.azure.com:v3/paidpiper2020/PaidPiper
-find . -type f -name "gitmodules" -print0 | xargs -0 sed -i 's+$SOURCE_URL+git@github.com:SCSI-9/test.git/+g' gitmodules
-cat gitmodules
+find . -type f -name "gitmodules" -print0 | xargs -0 sed -i 's+git@ssh.dev.azure.com:v3/paidpiper2020/PaidPiper+git@github.com:SCSI-9/test.git/+g'
